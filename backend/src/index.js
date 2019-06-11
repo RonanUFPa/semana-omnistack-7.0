51f2bea6e249ddf1b62b9bf1b7d817586f1a64c9
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-11 15:26:47 -0300

    Update index.js
    Configurando o ambiente node

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,9 @@
-const express = require(express);
+//inicializando o servidor com express
+const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('hello world');
+});
 
 app.listen(3333);
