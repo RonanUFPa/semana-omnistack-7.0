e37bf118f653ae848077706aa9e54d84b43bb0e1
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-17 22:10:51 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('')
+mongoose.connect('mongodb+srv://ronan:<4everayzer>@cluster0-hqoqg.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+})// conectando bd cluster
+
 
 app.get('/', (req, res) => {
   return res.send('hello world, 2019 aaaa'); //res representa a nossa reposta
