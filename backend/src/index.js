f1f398878a65fecff0c370af57af7c41ade34858
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-17 21:32:26 -0300

    att dev dependencies
    atualizações das dependencias de desenvolvedor

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send('hello world'); //res representa a nossa reposta
+  return res.send('hello world, 2019 aaaaa'); //res representa a nossa reposta
 });
 
 app.listen(3333); //ouvindo na porta 3333
