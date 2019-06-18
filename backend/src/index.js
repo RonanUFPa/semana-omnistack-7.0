f7f61155e8c50332d3c6d4089287598dcad6e9e0
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-17 22:04:21 -0300

    create mongoose dev
    criação do banco de dados para linkar no cluster

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 //inicializando o servidor com express
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('')
+
 app.get('/', (req, res) => {
-  return res.send('hello world, 2019 aaaaa'); //res representa a nossa reposta
+  return res.send('hello world, 2019 aaaa'); //res representa a nossa reposta
 });
 
 app.listen(3333); //ouvindo na porta 3333
