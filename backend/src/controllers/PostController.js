7fdc97ec71cb3bb648b7a1fc0121401d0c412bd1
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-18 15:21:09 -0300

    update api
    upload de imagem, listando post da aplicação

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -2,11 +2,21 @@ const Post = require('../models/Post');
 
 module.exports = {
   async index(req, res){
-  
+    const posts = await Post.find().sort('-createdAt');
+    return res.json(posts);
   },
   async store(req, res){
-    console.log(req.file); //verifica o envio da imagem com o .file e .body, do corpo da mensagem
-    return res.json({ ok: true }); // pra gente saber se o post deu tudo fine no insominia
+    const { author, place, description, hashtags} = req.body;
+    const { filename: image} = req.file;
+
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image,
+    });
+    return res.json(post); // pra gente saber se o post deu tudo fine no insominia
   }
   
 };
